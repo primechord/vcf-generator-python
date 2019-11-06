b0157c6c6c9ad8a09fa0a24d1aa4081aedfe1877
n.nedoseykin <nnedoseykin@ourgapps.com>
2019-11-06 12:18:16 +0300

    release script

---
+++ README.md
@@ -0,0 +1,10 @@
+Реализация экспорта списка контактов из Google Sheets в файл формата
+vcf. После этого контакты из файла можно экспортировать на мобильный
+телефон. 
+
+Пример кода для работы с Google Sheets API и процесс настройки
+аккаунта взят по [ссылке](https://www.youtube.com/watch?v=Bf8KHZtcxnA).
+
+Предварительно нужно установить зависимости через requirements.txt, а
+также изменить значения CREDENTIALS_FILE, spreadsheet_id,
+result_file_path, selected_range.
---
+++ vcf.py
@@ -0,0 +1,43 @@
+import httplib2
+import apiclient.discovery
+from oauth2client.service_account import ServiceAccountCredentials
+
+
+# Файл, полученный в Google Developer Console
+CREDENTIALS_FILE = 'creds.json'
+# ID Google Sheets документа (его можно взять из URL)
+spreadsheet_id = '12345678901234567890123456789012345678901234'
+
+# Авторизуемся и получаем service — экземпляр доступа к API
+credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    CREDENTIALS_FILE,
+    ['https://www.googleapis.com/auth/spreadsheets',
+     'https://www.googleapis.com/auth/drive'])
+httpAuth = credentials.authorize(httplib2.Http())
+service = apiclient.discovery.build('sheets', 'v4', http=httpAuth)
+
+
+# Получаем массивы, содержащие ФИО и номер телефона
+def get_rows(our_range):
+    values = service.spreadsheets().values().get(
+        spreadsheetId=spreadsheet_id,
+        range=our_range,
+        majorDimension='ROWS'
+    ).execute()
+    return values.get('values')
+
+
+# Записываем контакты в файл формата vcf
+def vcf_writer(result_file_path, selected_range):
+    new_file = open(result_file_path, 'w')
+    for person in get_rows(selected_range):
+        new_file.write("BEGIN:VCARD\n"
+                       "VERSION:2.1\n"
+                       "FN:%s %s\n"
+                       "TEL;TYPE=WORK:%s\n"
+                       "END:VCARD\n"
+                       % ('First event', person[0], person[1].strip("\n")))
+    new_file.close()
+
+
+vcf_writer(result_file_path='C:/Users/USERNAME/Desktop/result.vcf', selected_range='A1:B3')
